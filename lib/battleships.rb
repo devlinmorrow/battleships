bd3e12f10f6464c7ea3e6d6699538b53bdfe93d2
Devlin Glasman <devlin.glasman@gmail.com>
2018-02-14 15:26:02 +0000

    "Add hit or miss function with 3x3 grid"

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,36 +1,52 @@
 class Battleships
 
-  attr_accessor :box_top, :box_mid, :box_bot, :selected_box_number, :box_1_variable, :box_2_variable
+  attr_accessor :box_row_1, :box_row_2, :box_row_3, :box_row_4, :box_row_5, :box_row_6, :box_row_7, :box_row_8, :box_row_9, :selected_box_number, :box_1_variable, :box_2_variable, :box_3_variable, :box_4_variable, :box_5_variable, :box_6_variable, :box_7_variable, :box_8_variable, :box_9_variable
 
   def initialize
-    create_boxes
+    create_initial_board
   end
 
-  def construct_box
-    self.box_top = ["╔","═","═","═","═","═","╗""╔","═","═","═","═","═","╗"]
-
-    self.box_mid = ["║"," "," ",box_1_variable," "," ","║","║"," "," ",box_2_variable," "," ","║"]
-
-    self.box_bot = ["╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝"]
-
+  def construct_board
+    self.box_row_1 = ["╔","═","═","═","═","═","╗""╔","═","═","═","═","═","╗","╔","═","═","═","═","═","╗"]
+    self.box_row_2 = ["║"," "," ",box_1_variable," "," ","║","║"," "," ",box_2_variable," "," ","║","║"," "," ",box_3_variable," "," ","║"]
+    self.box_row_3 = ["╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝"]
+    self.box_row_4 = ["╔","═","═","═","═","═","╗""╔","═","═","═","═","═","╗","╔","═","═","═","═","═","╗"]
+    self.box_row_5 = ["║"," "," ",box_4_variable," "," ","║","║"," "," ",box_5_variable," "," ","║","║"," "," ",box_6_variable," "," ","║"]
+    self.box_row_6 = ["╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝"]
+    self.box_row_7 = ["╔","═","═","═","═","═","╗""╔","═","═","═","═","═","╗","╔","═","═","═","═","═","╗"]
+    self.box_row_8 = ["║"," "," ",box_7_variable," "," ","║","║"," "," ",box_8_variable," "," ","║","║"," "," ",box_9_variable," "," ","║"]
+    self.box_row_9 = ["╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝","╚","═","═","═","═","═","╝"]
   end
 
   def join_full_box
-    construct_box
-    @box_top_joined = self.box_top.join
-    @box_mid_joined = self.box_mid.join
-    @box_bot_joined = self.box_bot.join
-    @joined_box = "#{@box_top_joined}\n#{@box_mid_joined}\n#{@box_bot_joined}"
+    construct_board
+    box_row_1_joined = self.box_row_1.join
+    box_row_2_joined = self.box_row_2.join
+    box_row_3_joined = self.box_row_3.join
+    box_row_4_joined = self.box_row_4.join
+    box_row_5_joined = self.box_row_5.join
+    box_row_6_joined = self.box_row_6.join
+    box_row_7_joined = self.box_row_7.join
+    box_row_8_joined = self.box_row_8.join
+    box_row_9_joined = self.box_row_9.join
+    @joined_box = "#{box_row_1_joined}\n#{box_row_2_joined}\n#{box_row_3_joined}\n#{box_row_4_joined}\n#{box_row_5_joined}\n#{box_row_6_joined}\n#{box_row_7_joined}\n#{box_row_8_joined}\n#{box_row_9_joined}"
   end
 
-  def create_boxes
+  def create_initial_board
     self.box_1_variable = "1"
     self.box_2_variable = "2"
+    self.box_3_variable = "3"
+    self.box_4_variable = "4"
+    self.box_5_variable = "5"
+    self.box_6_variable = "6"
+    self.box_7_variable = "7"
+    self.box_8_variable = "8"
+    self.box_9_variable = "9"
     join_full_box
   end
 
   def display_board
-  puts @joined_box
+    puts @joined_box
   end
 
   def take_user_input
@@ -38,12 +54,28 @@ class Battleships
     self.selected_box_number = gets.chomp.to_i
   end
 
-  def mark_as_attacked_but_missed
-    self.box_mid[3] = "☠"
-  end
-
-  def mark_as_attacked_and_hit
-    self.box_mid[3] = "⚓"
+  def mark_as_hit_or_miss
+    case self.selected_box_number
+    when 1
+      self.box_1_variable = "☠"
+    when 2
+      self.box_2_variable = "⚓"
+    when 3
+      self.box_3_variable = "☠"
+    when 4
+      self.box_4_variable = "☠"
+    when 5
+      self.box_5_variable = "☠"
+    when 6
+      self.box_6_variable = "☠"
+    when 7
+      self.box_7_variable = "☠"
+    when 8
+      self.box_8_variable = "☠"
+    when 9
+      self.box_9_variable = "⚓"
+    end
+    join_full_box
   end
 
 end
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -2,32 +2,44 @@ require 'battleships'
 
 describe Battleships do
   subject(:example_game) {Battleships.new}
-  describe "display board" do
+  describe "set up  board" do
     it "should start with a box" do
-      expect(example_game.box_mid).to include("1")
+      expect(example_game.box_row_2).to include("1")
     end
     it "should display box 1" do
-          expect{example_game.display_board}.to output(/1/).to_stdout
+      expect{example_game.display_board}.to output(/1/).to_stdout
     end
-    it "should display box 2" do
-          expect{example_game.display_board}.to output(/2/).to_stdout
+    it "should display box 8" do
+      expect{example_game.display_board}.to output(/2/).to_stdout
     end
   end
   describe "user selects box" do
-    before do
-      $stdin = StringIO.new("1")
-    end
     it "should allow user to pick a number" do
+      $stdin = StringIO.new("1")
       example_game.take_user_input
       expect(example_game.selected_box_number).to eql(1)
     end
-    it "should mark the chosen number as attacked but missed" do
-      example_game.mark_as_attacked_but_missed
-      expect(example_game.box_mid[3]).to eq("☠")
+  end
+  describe "some boxes are hits some are misses" do
+    it "should mark box 1 as attacked but missed" do
+      example_game.selected_box_number = 1
+      example_game.mark_as_hit_or_miss
+      expect(example_game.box_row_2[3]).to eq("☠")
     end
-    it "should mark the chosen number as attacked and hit" do
-      example_game.mark_as_attacked_and_hit
-      expect(example_game.box_mid[3]).to eq("⚓")
+    it "should mark box 2 as attacked and hit" do
+      example_game.selected_box_number = 2
+      example_game.mark_as_hit_or_miss
+      expect(example_game.box_row_2[10]).to eq("⚓")
+    end
+    it "should mark box 4 as attacked but missed" do
+      example_game.selected_box_number = 4
+      example_game.mark_as_hit_or_miss
+      expect(example_game.box_row_5[3]).to eq("☠")
+    end
+    it "should mark box 9 as attacked and hit" do
+      example_game.selected_box_number = 9
+      example_game.mark_as_hit_or_miss
+      expect(example_game.box_row_8[17]).to eq("⚓")
     end
   end
 end
