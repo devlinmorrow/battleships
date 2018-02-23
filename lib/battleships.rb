f69fddb704a5f28144cd7cdb2947a79c821f02e0
Devlin Glasman <devlin.glasman@gmail.com>
2018-02-23 16:20:55 +0000

    working on creating boats

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -13,7 +13,7 @@ class Battleships
     @guesses_left = 20
     @game_is_won = false
     @letter_collection = ("A".."Z").to_a
-    @grid_size = 2
+    @grid_size = 10
     construct_initial_grid
     set_random_boats
   end
@@ -45,48 +45,69 @@ class Battleships
   end
 
   def set_random_boats
-    p @boat_1 = generate_boats_length_1
-    p @boat_2 = generate_boats_length_1
-    # p @boat_3 = generate_boats_length_1
-    # p @boat_4 = generate_boats_length_1
-    until ensure_no_boat_the_same(@boat_1,@boat_2)
-      @boat_2 = generate_boats_length_1
+    @boat_1 = generate_boat(1)
+    p @boat_1[0]
+    @boat_2 = generate_boat(1)
+    p @boat_2[0]
+    @boat_3 = generate_boat(1)
+    p @boat_3[0]
+    @boat_4 = generate_boat(1)
+    p @boat_4[0]
+    @boat_5 = generate_boat(2)
+    p @boat_5[0]
+    p @boat_5[1]
+   @boat_6 = generate_boat(2)
+   p @boat_6[0]
+   p @boat_6[1]
+   # @boat_7 = generate_boat(3)
+   # p @boat_7[0]
+   # p @boat_7[1]
+   # p @boat_7[2]
+   # @boat_8 = generate_boat(3)
+   # p @boat_8[0]
+   # p @boat_8[1]
+   # p @boat_8[2]
+   # @boat_9 = generate_boat(4)
+   # p @boat_9[0]
+   # p @boat_9[1]
+   # p @boat_9[2]
+   # p @boat_9[3]
+  end
+
+  def generate_boat(length)
+    boat_array = []
+    #Create the first coordinate of the boat, ensuring that the boat cannot extend further than the edges of the grid.
+    length.times do
+      r1 = rand(1..(@grid_size - length + 1))
+      c1 = rand(1..(@grid_size - length + 1))
+      boat_array << [r1,c1]
     end
-
-    p @boat_1
-    p @boat_2
-    # p @boat_3
-    # p @boat_4
-
-    #    #boat 5
-    #    b5r1 = rand(1..9)
-    #    b5c1 = rand(1..9)
-    #    @boat_5 = [[b5r1,b5c1],[b5r1,b5c1]]
-    #    x = rand(0..1)
-    #    @boat_5[1][x] = @boat_5[1][x] + 1
-    #    generate_boat_6
-    #    until (@boat_5 & @boat_6).empty?
-    #     generate_boat_6
-    #    end 
+    #Enlarge the boat to full size vertically or horizontally. 
+    v_or_h = 1
+    val = 1
+    while val < length 
+      boat_array[val][v_or_h] = boat_array[val][v_or_h] + val
+      val += 1
+    end
+    boat_array
   end
 
-  def generate_boats_length_1
-    r1 = rand(1..@grid_size)
-    c1 = rand(1..@grid_size)
-    [[r1,c1]]
-  end
+  #length = 2
+  #boat_array[1][0/1] = boat_array[1][0/1] + 1
+  #
+  #length = 3
+  #boat_array[1][0/1] = boat_array[1][0/1] + 1
+  #boat_array[2][0/1] = boat_array[2][0/1] + 2
+  #
+  #length = 4
+  #boat_array[1][0/1] = boat_array[1][0/1] + 1
+  #boat_array[2][0/1] = boat_array[2][0/1] + 2
+  #boat_array[3][0/1] = boat_array[3][0/1] + 3
+  #
 
-  def generate_boat_6
-    b6r1 = rand(1..9)
-    b6c1 = rand(1..9)
-    @boat_6 = [[b6r1,b6c1],[b6r1,b6c1]]
-    x = rand(0..1)
-    @boat_6[1][x] = @boat_6[1][x] + 1
-  end
-
-  def ensure_no_boat_the_same(boat1,boat2)
-    (boat1 & boat2).empty?
-  end
+  #def ensure_no_boat_the_same(boat1,boat2)
+  #  (boat1 & boat2).empty?
+  #end
 
   def convert_coordinates
     coordinates_array = @selected_box_coordinates.chars
