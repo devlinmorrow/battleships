9e111cb45f2eb091e9d731788d9fab7aefe184a2
Devlin Glasman <devlin.glasman@gmail.com>
2018-03-05 17:30:05 +0000

    Boat objects contain hash.

--- spec/battleships_boat_spec.rb
+++ spec/battleships_boat_spec.rb
@@ -10,6 +10,14 @@ describe Boat do
       end
     end
 
+    fit "sets coordinate value to 'true'" do
+        example_boat = Boat.new([[9,1]])
+
+        Boat.boat_coords_hit?([example_boat],[9,1]))
+
+        expect(example_boat.boat_coords_hash[9,1]).to eql(true)
+      end
+
     context "when boat coords not hit" do
       it "returns false" do
         example_boat = Boat.new([[9,1]])
@@ -21,7 +29,7 @@ describe Boat do
 
   describe "#boat_sunk?" do
     context "all boat coords hit" do
-      xit "sets boat_1 boat_sunk condition to true" do
+      it "sets boat_1 boat_sunk condition to true" do
         example_boat = Boat.new([[9,1]])
 
         expect(example_boat.boat_sunk?).to eql(true)
